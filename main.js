5204e1f816baf91cc9b62f4a26ac443a927d8123
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-08 18:28:04 +0100

    inserito ciclo per far inserire 5 numeri all'utente dopo 30 sec

--- main.js
+++ main.js
@@ -2,15 +2,16 @@
 // Visualizzare in pagina 5 numeri casuali.
 let wrapper = document.querySelector('.number-container');
 
-let pcNumberContainer = document.getElementsByClassName("random-number");
-
 let newNumber = [];
 for ( let i = 1; i <= 5; i++){
     newNumber = randomInt(1,100)
     console.log(newNumber)
-    pcNumberContainer.innerHTML = newNumber;
+    let numberBox = document.createElement('div');
+    numberBox.innerHTML = newNumber;
+    numberBox.classList.add('random-number');
+    wrapper.appendChild(numberBox)
+
 }
-console.log(pcNumberContainer)
 // genero una funzione per generare numeri random
 
 function randomInt (minValue, maxValue){
@@ -21,12 +22,26 @@ function randomInt (minValue, maxValue){
 };
 // Da lì parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-let userNumber = 0;
+let userNumber = [];
+let userScore = document.getElementById('user-score')
 function userinput (){
-    userNumber = parseInt(prompt("inserisci un numero"))
+    for ( let i = 0; i < 5; i++){
+        userNumber = parseInt(prompt("inserisci un numero"))
+        console.log(userNumber)
+        let UserNumberBox = document.createElement('div');
+        UserNumberBox.innerHTML = userNumber;
+        UserNumberBox.classList.add('random-number');
+        userScore.appendChild(UserNumberBox)
+    }
+}
+
+function clearNumbers (){
+    wrapper.innerHTML = '';
+
 }
 
 setTimeout(userinput, 3000)
+setTimeout(clearNumbers, 2900)
 
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
